901119f877091d4b9f81a1d29a17458d4d135a92
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-01-29 15:12:39 -0500

    added expect assertions

--- script2.test.js
+++ script2.test.js
@@ -1,16 +1,26 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
 
-it ('calls swapi to get people', (done) => {
+it ('calls swapi to get people', () => {
     expect.assertions(1)
-    swapi.getPeople(fetch).then(data => {
+    return swapi.getPeople(fetch).then(data => {
         expect(data.count).toEqual(82)
-        done();
     })
 })
 
+// it ('calls swapi to get people', (done) => {
+//     expect.assertions(1)
+//     swapi.getPeople(fetch).then(data => {
+//         expect(data.count).toEqual(82)
+//         done();
+//     })
+// })
+
 it ('calls swapi to get people with a promise', () => {
-    swapi.getPeoplePromise(fetch).then(data => {
+    expect.assertions(2)
+    return swapi.getPeoplePromise(fetch).then(data => {
+        //console.log(data.results)
         expect(data.count).toEqual(82)
+        expect(data.results.length).toBeGreaterThan(5)
     })
 })
